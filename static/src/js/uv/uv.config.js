42e980a37210df885d94fd6619aec97e0ae0af5d
DoomsickIe <doomsicklesgt72@gmail.com>
2022-09-24 02:56:38 -0400

    f

--- static/src/js/uv/uv.config.js
+++ static/src/js/uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
-    handler: 'https://combative-gold-moccasins.cyclic.app/src/js/uv/uv.handler.js',
-    bundle: 'https://combative-gold-moccasins.cyclic.app/src/js/uv/uv.bundle.js',
-    config: 'https://combative-gold-moccasins.cyclic.app/src/js/uv/uv.config.js',
-    sw: 'https://combative-gold-moccasins.cyclic.app/src/js/uv/uv.sw.js',
+    handler: '/src/js/uv/uv.handler.js',
+    bundle: '/src/js/uv/uv.bundle.js',
+    config: '/src/js/uv/uv.config.js',
+    sw: '/src/js/uv/uv.sw.js',
 };
--- static/src/js/uv/uv.sw.js
+++ static/src/js/uv/uv.sw.js
@@ -1,5 +1,5 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv/uv.config.js');
+importScripts('/src/js/uv/uv.bundle.js');
+importScripts('/src/js/uv/uv.config.js');
 
 class UVServiceWorker extends EventEmitter {     
     constructor(config = __uv$config) {
